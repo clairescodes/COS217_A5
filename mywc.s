5eb1d89c9a55fcad42a938f11dc1be6e50bfdfc9
Jeonghyun Shin <js9523@princeton.edu>
2024-11-22 11:25:10 -0500

    fixed errors while compiling

--- mywc.s
+++ mywc.s
@@ -32,100 +32,110 @@ fmt_string:
     .global main
 main:
     // Function prologue
-    stp x29, x30, [sp, -16]!    // Save frame pointer and return address
-    mov x29, sp                 // Set frame pointer
+    stp x29, x30, [sp, -16]!        // Save frame pointer and return address
+    mov x29, sp                     // Set frame pointer
+
+    // Compute addresses of global variables
+    adrp x20, iChar                 // Compute base address of iChar
+    add x20, x20, :lo12:iChar       // Get full address of iChar
+
+    adrp x21, iInWord               // Compute base address of iInWord
+    add x21, x21, :lo12:iInWord     // Get full address of iInWord
+
+    adrp x22, lLineCount            // Compute base address of lLineCount
+    add x22, x22, :lo12:lLineCount  // Get full address of lLineCount
+
+    adrp x23, lWordCount            // Compute base address of lWordCount
+    add x23, x23, :lo12:lWordCount  // Get full address of lWordCount
+
+    adrp x24, lCharCount            // Compute base address of lCharCount
+    add x24, x24, :lo12:lCharCount  // Get full address of lCharCount
+
+    adrp x25, fmt_string            // Compute base address of fmt_string
+    add x25, x25, :lo12:fmt_string  // Get full address of fmt_string
 
     /* while ((iChar = getchar()) != EOF) */
 Loop_Start:
-    bl getchar                  // iChar = getchar();
-    ldr x0, =iChar              // Load address of iChar
-    str w0, [x0]                // Store iChar in memory
+    bl getchar                      // Call getchar()
+    str w0, [x20]                   // Store iChar in memory
 
-    ldr w1, [x0]                // Load iChar into w1
-    cmp w1, #-1                 // if (iChar == EOF)
-    beq Loop_End                // Break if end of file
+    ldr w1, [x20]                   // Load iChar into w1
+    cmp w1, #-1                     // Compare iChar with EOF (-1)
+    beq Loop_End                    // If iChar == EOF, exit loop
 
     /* lCharCount = lCharCount + 1; */
-    ldr x1, =lCharCount         // Load address of lCharCount
-    ldr x2, [x1]                // Load current value of lCharCount
-    add x2, x2, #1              // lCharCount += 1
-    str x2, [x1]                // Store updated lCharCount
+    ldr x2, [x24]                   // Load lCharCount
+    add x2, x2, #1                  // Increment lCharCount
+    str x2, [x24]                   // Store updated lCharCount
 
     /* if (isspace(iChar)) */
-    mov w0, w1                  // Prepare argument for isspace(iChar)
-    bl isspace                  // Call isspace()
-    cmp w0, #0                  // Compare result with 0
-    beq NotSpace                // If zero, character is not whitespace
+    mov w0, w1                      // Move iChar into w0 for isspace
+    bl isspace                      // Call isspace(iChar)
+    cmp w0, #0                      // Compare result with 0
+    beq NotSpace                    // If zero, character is not whitespace
 
-    /*     if (iInWord) */
-    ldr x3, =iInWord            // Load address of iInWord
-    ldr w3, [x3]                // Load value of iInWord
-    cmp w3, #0
-    beq SkipWordCountIncrement  // Skip if iInWord == FALSE
+        /* if (iInWord) */
+        ldr w3, [x21]               // Load iInWord
+        cmp w3, #0                  // Compare iInWord with 0
+        beq SkipWordCountIncrement  // If iInWord == FALSE, skip
 
-        /* lWordCount = lWordCount + 1; */
-        ldr x4, =lWordCount     // Load address of lWordCount
-        ldr x5, [x4]            // Load current value of lWordCount
-        add x5, x5, #1          // lWordCount += 1
-        str x5, [x4]            // Store updated lWordCount
+            /* lWordCount = lWordCount + 1; */
+            ldr x5, [x23]           // Load lWordCount
+            add x5, x5, #1          // Increment lWordCount
+            str x5, [x23]           // Store updated lWordCount
 
-        /* iInWord = FALSE; */
-        mov w3, #0              // Set iInWord to FALSE
-        str w3, [x3]            // Store updated iInWord
+            /* iInWord = FALSE; */
+            mov w3, #0              // Set iInWord to FALSE
+            str w3, [x21]           // Store updated iInWord
 
 SkipWordCountIncrement:
-    b CheckNewline              // Proceed to check newline
+    b CheckNewline                  // Proceed to check newline
 
 NotSpace:
     /* else if (!iInWord) */
-    ldr w3, [x3]                // Load value of iInWord
-    cmp w3, #0
-    bne CheckNewline            // Skip if iInWord == TRUE
+    ldr w3, [x21]                   // Load iInWord
+    cmp w3, #0                      // Compare iInWord with 0
+    bne CheckNewline                // If iInWord != FALSE, skip
 
         /* iInWord = TRUE; */
-        mov w3, #1              // Set iInWord to TRUE
-        str w3, [x3]            // Store updated iInWord
+        mov w3, #1                  // Set iInWord to TRUE
+        str w3, [x21]               // Store updated iInWord
 
 CheckNewline:
     /* if (iChar == '\n') */
-    cmp w1, #10                 // Compare iChar with newline character
-    bne Loop_Next               // Skip if not newline
+    cmp w1, #10                     // Compare iChar with newline character
+    bne Loop_Next                   // If not newline, skip
 
         /* lLineCount = lLineCount + 1; */
-        ldr x6, =lLineCount     // Load address of lLineCount
-        ldr x7, [x6]            // Load current value of lLineCount
-        add x7, x7, #1          // lLineCount += 1
-        str x7, [x6]            // Store updated lLineCount
+        ldr x7, [x22]               // Load lLineCount
+        add x7, x7, #1              // Increment lLineCount
+        str x7, [x22]               // Store updated lLineCount
 
 Loop_Next:
-    b Loop_Start                // Repeat loop
+    b Loop_Start                    // Repeat loop
 
 Loop_End:
     /* if (iInWord) */
-    ldr x3, =iInWord            // Load address of iInWord
-    ldr w3, [x3]                // Load value of iInWord
-    cmp w3, #0
-    beq AfterFinalWordCount     // Skip if iInWord == FALSE
+    ldr w3, [x21]                   // Load iInWord
+    cmp w3, #0                      // Compare iInWord with 0
+    beq AfterFinalWordCount         // If iInWord == FALSE, skip
 
-    /* lWordCount = lWordCount + 1; */
-    ldr x4, =lWordCount         // Load address of lWordCount
-    ldr x5, [x4]                // Load current value of lWordCount
-    add x5, x5, #1              // lWordCount += 1
-    str x5, [x4]                // Store updated lWordCount
+        /* lWordCount = lWordCount + 1; */
+        ldr x5, [x23]               // Load lWordCount
+        add x5, x5, #1              // Increment lWordCount
+        str x5, [x23]               // Store updated lWordCount
 
 AfterFinalWordCount:
     /* printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount); */
-    adrp x0, fmt_string         // Load page address of fmt_string
-    add x0, x0, #:lo12:fmt_string // Add low 12 bits of the address
-    ldr x1, =lLineCount         // Load address of lLineCount
-    ldr x1, [x1]                // Load value of lLineCount
-    ldr x2, =lWordCount         // Load address of lWordCount
-    ldr x2, [x2]                // Load value of lWordCount
-    ldr x3, =lCharCount         // Load address of lCharCount
-    ldr x3, [x3]                // Load value of lCharCount
-    bl printf                   // Call printf()
+    mov x0, x25                     // Move address of fmt_string to x0
+
+    ldr x1, [x22]                   // Load lLineCount
+    ldr x2, [x23]                   // Load lWordCount
+    ldr x3, [x24]                   // Load lCharCount
+
+    bl printf                       // Call printf()
 
     /* return 0; */
-    mov w0, #0                  // Prepare return value
-    ldp x29, x30, [sp], #16     // Restore frame pointer and return address
-    ret                         // Return from main
+    mov w0, #0                      // Set return value to 0
+    ldp x29, x30, [sp], #16         // Restore frame pointer and return address
+    ret                             // Return from main
