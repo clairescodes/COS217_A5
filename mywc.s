6e6852f32a6b41702b224f4221e95567a489eff8
Jeonghyun Shin <js9523@princeton.edu>
2024-11-28 21:37:56 -0500

    debugged mywc

--- mywc.s
+++ mywc.s
@@ -5,7 +5,7 @@
 
         .section .rodata
 
-fmt_string: 
+fmt_string:
         .string "%7ld %7ld %7ld\n" // Format string for printf
 
 //---------------------------------------------------------------------
@@ -36,45 +36,53 @@ iInWord:         .word 0
 
         .section .text
 
+        // Define constants
+        .equ EOF, -1              // End of file
+        .equ TRUE, 1              // Boolean true
+        .equ FALSE, 0             // Boolean false
+        .equ NEWLINE, 10          // ASCII code for '\n'
+        .equ STACK_FRAME_SIZE, 16 // Stack frame size (aligned to 16 bytes)
+
         .global main
 
 main:
         // Prolog
-        sub     sp, sp, #16       // Allocate stack frame
-        str     x30, [sp, #8]     // Save return address
-        mov     x29, sp           // Set frame pointer
+        sub     sp, sp, #STACK_FRAME_SIZE // Allocate stack frame
+        str     x30, [sp]                 // Save return address on the stack
+        mov     x29, sp                   // Set frame pointer
 
         // while ((iChar = getchar()) != EOF)
 Loop_Start:
         bl      getchar           // Call getchar()
-        str     w0, iChar         // iChar = getchar()
+        str     w0, iChar         // Store iChar = getchar()
+
         ldr     w1, iChar         // Load iChar
-        cmp     w1, #-1           // Compare iChar with EOF
+        cmp     w1, #EOF          // Compare iChar with EOF
         beq     Loop_End          // Exit loop if iChar == EOF
 
         // lCharCount++;
         ldr     x2, lCharCount    // Load lCharCount
-        add     x2, x2, #1        // Increment lCharCount
+        add     x2, x2, TRUE      // Increment lCharCount by TRUE (1)
         str     x2, lCharCount    // Store updated lCharCount
 
         // if (isspace(iChar))
         mov     w0, w1            // Move iChar into w0 for isspace
         bl      isspace           // Call isspace(iChar)
-        cmp     w0, #0            // Compare result with 0
+        cmp     w0, FALSE         // Compare result with FALSE (0)
         beq     NotSpace          // If not whitespace, skip
 
         // if (iInWord)
         ldr     w3, iInWord       // Load iInWord
-        cmp     w3, #0            // Compare iInWord with FALSE
+        cmp     w3, FALSE         // Compare iInWord with FALSE
         beq     SkipWordCountIncrement // Skip if iInWord == FALSE
 
         // lWordCount++;
         ldr     x5, lWordCount    // Load lWordCount
-        add     x5, x5, #1        // Increment lWordCount
+        add     x5, x5, TRUE      // Increment lWordCount by TRUE (1)
         str     x5, lWordCount    // Store updated lWordCount
 
         // iInWord = FALSE;
-        mov     w3, #0            // Set iInWord to FALSE
+        mov     w3, FALSE         // Set iInWord to FALSE
         str     w3, iInWord       // Store updated iInWord
 
 SkipWordCountIncrement:
@@ -83,21 +91,21 @@ SkipWordCountIncrement:
 NotSpace:
         // else if (!iInWord)
         ldr     w3, iInWord       // Load iInWord
-        cmp     w3, #0            // Compare iInWord with FALSE
+        cmp     w3, FALSE         // Compare iInWord with FALSE
         bne     CheckNewline      // If iInWord != FALSE, skip
 
         // iInWord = TRUE;
-        mov     w3, #1            // Set iInWord to TRUE
+        mov     w3, TRUE          // Set iInWord to TRUE
         str     w3, iInWord       // Store updated iInWord
 
 CheckNewline:
         // if (iChar == '\n')
-        cmp     w1, #10           // Compare iChar with newline character
+        cmp     w1, #NEWLINE      // Compare iChar with newline character
         bne     Loop_Next         // Skip if not newline
 
         // lLineCount++;
         ldr     x7, lLineCount    // Load lLineCount
-        add     x7, x7, #1        // Increment lLineCount
+        add     x7, x7, TRUE      // Increment lLineCount by TRUE (1)
         str     x7, lLineCount    // Store updated lLineCount
 
 Loop_Next:
@@ -106,24 +114,24 @@ Loop_Next:
 Loop_End:
         // if (iInWord)
         ldr     w3, iInWord       // Load iInWord
-        cmp     w3, #0            // Compare iInWord with FALSE
+        cmp     w3, FALSE         // Compare iInWord with FALSE
         beq     AfterFinalWordCount // Skip if iInWord == FALSE
 
         // lWordCount++;
         ldr     x5, lWordCount    // Load lWordCount
-        add     x5, x5, #1        // Increment lWordCount
+        add     x5, x5, TRUE      // Increment lWordCount by TRUE (1)
         str     x5, lWordCount    // Store updated lWordCount
 
 AfterFinalWordCount:
         // printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
-        ldr     x0, =fmt_string   // Address of format string
+        ldr     x0, fmt_string    // Address of format string
         ldr     x1, lLineCount    // Load lLineCount
         ldr     x2, lWordCount    // Load lWordCount
         ldr     x3, lCharCount    // Load lCharCount
         bl      printf            // Call printf()
 
         // return 0;
-        mov     w0, #0            // Set return value to 0
-        ldr     x30, [sp, #8]     // Restore return address
-        add     sp, sp, #16       // Deallocate stack frame
-        ret
+        mov     w0, FALSE         // Set return value to FALSE (0)
+        ldr     x30, [sp]         // Restore return address from the stack
+        add     sp, sp, #STACK_FRAME_SIZE // Deallocate stack frame
+        ret                       // Return
