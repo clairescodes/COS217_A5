621322f897f845fa67a4f36f53e58c2e695d4f14
Jeonghyun Shin <js9523@princeton.edu>
2024-12-01 20:15:15 -0500

    bigintadd fix

--- bigintadd.s
+++ bigintadd.s
@@ -1,59 +1,3 @@
-/* bigintadd.s */
-
-/*--------------------------------------------------------------------*/
-/* Constants and Offsets                                              */
-/*--------------------------------------------------------------------*/
-
-    .equ    MAX_DIGITS, 32768
-    .equ    TRUE, 1
-    .equ    FALSE, 0
-
-    /* Offsets relative to x29 (frame pointer) */
-    /* Local variables and parameters stored at negative offsets */
-    .equ    ULCARRY, -8           // unsigned long ulCarry;
-    .equ    LINDEX, -16           // long lIndex;
-    .equ    LSUMLENGTH, -24       // long lSumLength;
-    .equ    OADDEND1, -32         // BigInt_T oAddend1;
-    .equ    OADDEND2, -40         // BigInt_T oAddend2;
-    .equ    OSUM, -48             // BigInt_T oSum;
-    /* Total frame size: 48 bytes (must be multiple of 16) */
-
-    /* Offsets within the BigInt_T structure */
-    .equ    LLENGTH, 0            // Offset of lLength in BigInt_T
-    .equ    AULDIGITS, 8          // Offset of aulDigits in BigInt_T
-
-    .global BigInt_larger
-    .global BigInt_add
-
-/*--------------------------------------------------------------------*/
-/* BigInt_larger: Returns the larger of lLength1 and lLength2         */
-/*--------------------------------------------------------------------*/
-BigInt_larger:
-    /* Prologue */
-    stp     x29, x30, [sp, #-16]!      // Save x29 and x30, adjust sp
-    mov     x29, sp                    // Set frame pointer
-
-    /* Allocate stack space for local variables (none needed here) */
-
-    /* Compare lLength1 and lLength2 */
-    cmp     x0, x1
-    bgt     larger_lLength1            // If lLength1 > lLength2, branch
-
-    /* lLength2 is larger or equal */
-    mov     x0, x1                     // Return lLength2
-    b       return_larger
-
-larger_lLength1:
-    /* lLength1 is larger */
-    /* x0 already contains lLength1 */
-
-return_larger:
-    /* Epilogue */
-    ldp     x29, x30, [sp], #16        // Restore x29 and x30, adjust sp
-    ret                                 // Return from function
-
-    .size BigInt_larger, . - BigInt_larger
-
 /*--------------------------------------------------------------------*/
 /* BigInt_add: Adds two BigInt numbers                                */
 /* Returns TRUE (1) if addition is successful, FALSE (0) if overflow  */
@@ -69,6 +13,15 @@ BigInt_add:
     str     x1, [x29, OADDEND2]        // Store oAddend2
     str     x2, [x29, OSUM]            // Store oSum
 
+    /* Ensure OSUM's aulDigits is cleared */
+    ldr     x0, [x29, OSUM]            // Load OSUM
+    mov     x1, MAX_DIGITS             // Initialize loop counter
+clear_osum_loop:
+    add     x2, x0, AULDIGITS          // x2 = address of OSUM->aulDigits
+    str     xzr, [x2], #8              // Clear 8 bytes (set to zero)
+    subs    x1, x1, #1                 // Decrement counter
+    bne     clear_osum_loop            // Repeat if not zero
+
     /* Determine lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength) */
     ldr     x0, [x29, OADDEND1]        // Load oAddend1
     ldr     x0, [x0, LLENGTH]          // x0 = oAddend1->lLength
@@ -78,7 +31,7 @@ BigInt_add:
     str     x0, [x29, LSUMLENGTH]      // Store lSumLength
 
     /* Initialize ulCarry = 0 and lIndex = 0 */
-    mov     x0, 0
+    mov     x0, 0                      // Reset carry and index
     str     x0, [x29, ULCARRY]
     str     x0, [x29, LINDEX]
 
@@ -189,5 +142,3 @@ end_BigInt_add:
     add     sp, sp, #48                // Deallocate local variables
     ldp     x29, x30, [sp], #16        // Restore x29 and x30, adjust sp
     ret                                 // Return from function
-
-    .size BigInt_add, . - BigInt_add
