00f330ea683a1ba8cb99ef0688511b0800622740
Jeonghyun Shin <js9523@princeton.edu>
2024-12-02 21:04:48 -0500

    update bigintadd

--- bigintaddopt.s
+++ bigintaddopt.s
@@ -1,154 +1,177 @@
-/* bigintaddopt.s */
+//---------------------------------------------------------------------
+// bigintaddopt.s
+// Author: Claire Shin and Emily Qian
+//---------------------------------------------------------------------
+        .section .rodata
 
-/*--------------------------------------------------------------------*/
-/* Constants and Offsets                                              */
-/*--------------------------------------------------------------------*/
+//----------------------------------------------------------------------
+        .section .data
 
-    .equ    MAX_DIGITS, 32768
-    .equ    TRUE, 1
-    .equ    FALSE, 0
-    .equ    LLENGTH, 0            // Offset of lLength in BigInt_T
-    .equ    AULDIGITS, 8          // Offset of aulDigits in BigInt_T
+//----------------------------------------------------------------------
+        .section .bss
 
-    .global BigInt_larger
-    .global BigInt_add
+//----------------------------------------------------------------------
+        .section .text
 
-/*--------------------------------------------------------------------*/
-/* BigInt_larger: Returns the larger of lLength1 and lLength2         */
-/*--------------------------------------------------------------------*/
+        // always multiple of 16 
+        .equ    BIGINT_STACK_BYTECOUNT, 32
 
-/* Register aliases */
-LLENGTH1    .req    x19
-LLENGTH2    .req    x20
+        // save past values so that they can be reverted back 
+        .equ    OLDX19, 
+        .equ    OLDX20, 
+        .equ    OLDX21, 
+
+
+
+        
+
+
+//---------------------------------------------------------------------
+// BigInt_larger
+//---------------------------------------------------------------------
+
+        .global BigInt_larger
 
 BigInt_larger:
-    /* Prologue */
-    stp     x29, x30, [sp, #-16]!      // Save x29 and x30
-    stp     LLENGTH1, LLENGTH2, [sp, #-16]! // Save callee-saved registers
-    mov     x29, sp                    // Set frame pointer
+        // Prolog
+        sub     sp, sp, 16
+        str     x30, [sp]
 
-    /* Move parameters into registers */
-    mov     LLENGTH1, x0
-    mov     LLENGTH2, x1
+        // Store parameters in bss
+        adr     x1, lLength1
+        str     x0, [x1]               // lLength1 = x0
+        adr     x1, lLength2
+        str     x1, [x1]               // lLength2 = x1
 
-    /* Compare lLength1 and lLength2 */
-    cmp     LLENGTH1, LLENGTH2
-    bgt     larger_lLength1            // If lLength1 > lLength2, branch
+        // Compare lLength1 and lLength2
+        adr     x0, lLength1
+        ldr     x0, [x0]
+        adr     x1, lLength2
+        ldr     x1, [x1]
+        cmp     x0, x1
+        adr     x2, lLarger
+        csel    x0, x0, x1, gt         // lLarger = max(lLength1, lLength2)
+        str     x0, [x2]
 
-    /* lLength2 is larger or equal */
-    mov     x0, LLENGTH2               // Return lLength2
-    b       return_larger
+        // Return lLarger
+        adr     x0, lLarger
+        ldr     x0, [x0]
 
-larger_lLength1:
-    mov     x0, LLENGTH1               // Return lLength1
+        // Epilog
+        ldr     x30, [sp]
+        add     sp, sp, 16
+        ret
 
-return_larger:
-    /* Epilogue */
-    ldp     LLENGTH1, LLENGTH2, [sp], #16 // Restore callee-saved registers
-    ldp     x29, x30, [sp], #16        // Restore x29 and x30
-    ret                                // Return from function
+        .size BigInt_larger, (. - BigInt_larger)
 
-    .size BigInt_larger, . - BigInt_larger
+//---------------------------------------------------------------------
+// BigInt_add
+//---------------------------------------------------------------------
 
-/*--------------------------------------------------------------------*/
-/* BigInt_add: Adds two BigInt numbers                                */
-/* Returns TRUE (1) if addition is successful, FALSE (0) if overflow  */
-/*--------------------------------------------------------------------*/
-
-/* Register aliases */
-OADDEND1    .req    x19
-OADDEND2    .req    x20
-OSUM        .req    x21
-ULCARRY     .req    x22
-LINDEX      .req    x23
-LSUMLENGTH  .req    x24
+        .global BigInt_add
 
 BigInt_add:
-    /* Prologue */
-    stp     x29, x30, [sp, #-16]!      // Save x29 and x30
-    stp     OADDEND1, OADDEND2, [sp, #-16]! // Save callee-saved registers
-    stp     OSUM, ULCARRY, [sp, #-16]!
-    stp     LINDEX, LSUMLENGTH, [sp, #-16]!
-    mov     x29, sp                    // Set frame pointer
+        // Prolog
+        sub     sp, sp, 64             // Allocate stack frame (multiple of 16)
+        str     x30, [sp]              // Save link register
 
-    /* Move parameters into registers */
-    mov     OADDEND1, x0
-    mov     OADDEND2, x1
-    mov     OSUM, x2
+        // Initialize ulCarry = 0 and lIndex = 0
+        adr     x0, ulCarry
+        mov     x1, xzr
+        str     x1, [x0]               // ulCarry = 0
+        adr     x0, lIndex
+        str     x1, [x0]               // lIndex = 0
 
-    /* Zero-initialize oSum->aulDigits */
-    add     x0, OSUM, AULDIGITS        // x0 = &oSum->aulDigits
-    mov     x1, MAX_DIGITS             // x1 = MAX_DIGITS
-    mov     x2, 0                      // x2 = 0 (value to store)
-zero_init_loop:
-    cbz     x1, zero_init_done         // If x1 == 0, exit loop
-    str     x2, [x0], #8               // Store 0 and increment pointer by 8
-    sub     x1, x1, 1                  // Decrement counter
-    b       zero_init_loop
-zero_init_done:
+        // Compute lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength)
+        adr     x1, AULDIGITS
+        ldr     x2, [x1]               // Load AULDIGITS value
+        ldr     x0, [x0, x2]           // Load oAddend1->lLength
+        ldr     x1, [x1, x2]           // Load oAddend2->lLength
+        bl      BigInt_larger          // Call BigInt_larger
+        adr     x2, lSumLength
+        str     x0, [x2]               // Store lSumLength
 
-    /* Determine lSumLength = BigInt_larger(oAddend1->lLength, oAddend2->lLength) */
-    ldr     x0, [OADDEND1, LLENGTH]    // x0 = oAddend1->lLength
-    ldr     x1, [OADDEND2, LLENGTH]    // x1 = oAddend2->lLength
-    bl      BigInt_larger              // Call BigInt_larger(x0, x1)
-    mov     LSUMLENGTH, x0             // Store lSumLength
-
-    /* Initialize ulCarry = 0 and lIndex = 0 */
-    mov     ULCARRY, 0
-    mov     LINDEX, 0
+        // Clear oSum->aulDigits using memset
+        // memset(oSum->aulDigits, 0, MAX_DIGITS * ..)
+        adr     x0, MAX_DIGITS
+        ldr     x1, [x0]               // Load MAX_DIGITS value
+        adr     x2, AULDIGITS
+        ldr     x3, [x2]               // Load AULDIGITS value
+        bl      memset
 
 addition_loop:
-    /* lIndex < lSumLength? */
-    cmp     LINDEX, LSUMLENGTH
-    bge     end_addition_loop
+        // Check if lIndex < lSumLength
+        adr     x0, lIndex
+        ldr     x0, [x0]               // Load lIndex
+        adr     x1, lSumLength
+        ldr     x1, [x1]               // Load lSumLength
+        cmp     x0, x1
+        bge     end_addition_loop      // Break if lIndex >= lSumLength
 
-    /* Add with carry */
-    ldr     x3, [OADDEND1, AULDIGITS + LINDEX, LSL #3] // Load oAddend1->aulDigits[lIndex]
-    ldr     x4, [OADDEND2, AULDIGITS + LINDEX, LSL #3] // Load oAddend2->aulDigits[lIndex]
-    adds    x6, ULCARRY, x3            // Add ulCarry + oAddend1->aulDigits[lIndex]
-    adcs    x6, x6, x4                 // Add oAddend2->aulDigits[lIndex] + carry
-    cset    ULCARRY, cs                // Update carry flag
+        // ulSum = ulCarry
+        adr     x0, ulCarry
+        ldr     x1, [x0]               // Load ulCarry
+        adr     x2, ulSum
+        str     x1, [x2]               // Store ulSum = ulCarry
 
-    /* Store ulSum in oSum->aulDigits[lIndex] */
-    str     x6, [OSUM, AULDIGITS + LINDEX, LSL #3]
-
-    /* Increment lIndex */
-    add     LINDEX, LINDEX, 1
-    b       addition_loop
+        // Add oAddend1->aulDigits[lIndex] to ulSum
+        adr     x1, AULDIGITS
+        ldr     x2, [x1]               // Load AULDIGITS value
+        lsl     x3, x0, x2             // lIndex * AULDIGITS
+        add     x4, x3, x0             // Address of oAddend1->aulDigits[lIndex]
+        ldr     x5, [x4]
+        adds    x5, x5, x1             // Add value
+        str     x5, [x1]
 
 end_addition_loop:
-    /* Check if ulCarry == 1 */
-    cbz     ULCARRY, set_sum_length    // Skip if ulCarry == 0
+        ldr     x7, x1
+        // Check for final carry
+        adr     x0, ulCarry
+        ldr     x0, [x0]
+        cbz     x0, set_sum_length    // If ulCarry == 0, skip to set_sum_length
 
-    /* Handle carry overflow */
-    cmp     LSUMLENGTH, MAX_DIGITS - 1
-    bgt     returnFalse                // If lSumLength > MAX_DIGITS - 1, return FALSE
+        // Check if there's room for an additional digit
+        adr     x1, lSumLength
+        ldr     x1, [x1]
+        adr     x2, MAX_DIGITS
+        ldr     x2, [x2]
+        cmp     x1, x2
+        bge     return_false          // If lSumLength == MAX_DIGITS, return FALSE
 
-    /* oSum->aulDigits[lSumLength] = 1 */
-    add     x8, OSUM, AULDIGITS        // x8 = &oSum->aulDigits
-    add     x8, x8, LSUMLENGTH, LSL #3 // x8 = &oSum->aulDigits[lSumLength]
-    mov     x6, 1
-    str     x6, [x8]                   // oSum->aulDigits[lSumLength] = 1
-
-    /* Increment lSumLength */
-    add     LSUMLENGTH, LSUMLENGTH, 1
+        // Add carry to oSum->aulDigits[lSumLength]
+        adr     x3, AULDIGITS
+        ldr     x4, [x3]              // Load AULDIGITS value
+        adr     x0, oSum
+        add     x0, x0, x4            // Address of oSum->aulDigits
+        lsl     x1, x1, x4            // lSumLength * AULDIGITS
+        add     x0, x0, x1
+        mov     x2, 1
+        str     x2, [x0]              // Store carry
+        adr     x1, lSumLength
+        ldr     x2, [x1]
+        add     x2, x2, 1
+        str     x2, [x1]              // Increment lSumLength
 
 set_sum_length:
-    /* Set oSum->lLength = lSumLength */
-    str     LSUMLENGTH, [OSUM, LLENGTH]
+        // Set oSum->lLength = lSumLength
+        adr     x0, oSum
+        adr     x1, lSumLength
+        ldr     x2, [x1]
+        adr     x3, AULDIGITS
+        ldr     x4, [x3]
+        add     x0, x0, x4            // Address of oSum->lLength
+        str     x2, [x0]
 
-    /* Return TRUE */
-    mov     x0, TRUE
-    b       end_BigInt_add
+        mov     x0, xzr               // Return TRUE
+        b       end_BigInt_add
 
-returnFalse:
-    /* Return FALSE */
-    mov     x0, FALSE
+return_false:
+        mov     x0, xzr               // Return FALSE
 
 end_BigInt_add:
-    /* Epilogue */
-    ldp     LINDEX, LSUMLENGTH, [sp], #16 // Restore callee-saved registers
-    ldp     OSUM, ULCARRY, [sp], #16
-    ldp     OADDEND1, OADDEND2, [sp], #16
-    ldp     x29, x30, [sp], #16        // Restore x29 and x30
-    ret                                // Return from function
+        // Epilog
+        ldr     x30, [sp]
+        add     sp, sp, 64            // Restore stack frame
+        ret
+
+        .size BigInt_add, (. - BigInt_add)
